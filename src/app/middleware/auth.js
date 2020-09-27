83b6b848d6cab6ed6598ef904120f30331a54ab9
Jefferson da Silva <je_jss@hotmail.com>
2020-09-27 14:26:45 -0300

    :hammer: update message errors

--- src/app/middleware/auth.js
+++ src/app/middleware/auth.js
@@ -10,12 +10,12 @@ module.exports = (request, response, next) => {
     const parts = authHeader.split(' ');
 
     if (!parts.length === 2)
-      return response.status(401).json({ error: 'Token error' });
+      return response.status(401).json({ error: 'Erro no Token' });
 
     const [ bearer, token ] = parts;
 
     if (!/^Bearer$/i.test(bearer))
-      return response.status(401).json({ error: 'Token malformatted' });
+      return response.status(401).json({ error: 'Token Mal Formatado' });
 
     jwt.verify(token, authConfig.secrect, (error, decoded) => {
       if (error) return response.status(401).json({ error: 'Não autorizado'});
