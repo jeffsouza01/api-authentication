5a8631c421106361a60695e1d79926d94c5be1df
Jefferson da Silva <je_jss@hotmail.com>
2020-10-30 03:00:51 -0300

    :star2: update schema name

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -53,6 +53,6 @@ UserSchema.pre('save', async function(next) {
     next();
 })
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model('Usuario', UserSchema);
 
 module.exports = User;
