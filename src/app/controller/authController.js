32fe27185f360d0dd649a922d59c8cfb2adba6d0
Jefferson da Silva <je_jss@hotmail.com>
2020-10-30 03:09:45 -0300

    :star2: include yup validation and destructureobject

--- src/app/controller/authController.js
+++ src/app/controller/authController.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
+const Yup = require('yup');
 
 const authConfig = require('../../config/auth.json');
 
@@ -22,7 +23,48 @@ module.exports = {
         }
 
         try {
-            const usuario = await User.create(request.body);
+            const {
+              nome,
+              email,
+              senha,
+              telefone: [
+                numero,
+                ddd,
+              ],
+              data_criacao,
+              data_atualizacao,
+              ultimo_login,
+            } = request.body;
+
+
+            const usuario = {
+              nome,
+              email,
+              senha,
+              telefone: [
+                numero,
+                ddd,
+              ],
+              data_criacao,
+              data_atualizacao,
+              ultimo_login,
+            }
+
+            const schema = Yup.object().shape({
+              nome: Yup.string().required(),
+              email: Yup.string().email().required(),
+              senha: Yup.string().required(),
+              telefone: Yup.array(
+                Yup.object().shape({
+                  numero: Yup.string().max(9).required(),
+                  ddd: Yup.string().max(2).required(),
+                })
+              ).compact()
+            })
+
+            await schema.validate(usuario);
+
+            await User.create(usuario);
 
             usuario.senha = undefined;
 
@@ -45,8 +87,7 @@ module.exports = {
           if (!usuario || !await bcrypt.compare(senha, usuario.senha))
             return response.status(401).json({ error: 'Usuário e/ou senha inválidos'});
 
-
-          now = new Date();
+          const now = new Date();
 
           await User.findByIdAndUpdate(usuario.id, {ultimo_login: now} , { upsert: true} );
 
